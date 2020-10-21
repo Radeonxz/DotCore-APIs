aafd25629692633f27a11c74ed202d0d2f277233
Radeonxz <xuanxxzhao@gmail.com>
2020-10-20 22:26:07 -0400

    chore: add Book and applicationDbContext

---
+++ BookleetMVC/BookleetMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BookleetMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
