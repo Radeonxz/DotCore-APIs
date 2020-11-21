ed0630368b117fe51537ecbfbc40f945c3155b58
Radeonxz <xuanxxzhao@gmail.com>
2020-11-20 23:19:17 -0500

    chore: controller wip

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Commander.Data;
+using commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -8,17 +11,23 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly MockCoommanderRepo _repository = new MockCommanderRepo();
+        // GET api/commands
         [HttpGet]
         public ActionResult <IEnumerable<Commander>> GetAllCommands()
         {
+            var commandItems = _repository.GetAllCommands();
 
+            return Ok(commandItems);
         }
 
         // GET api/commands/1
         [HttpGet("{id}")]
         public ActionResult <Commander> GetCommandById(int id)
         {
+            var commandItems = _repository.GetCommandById(id);
 
+            return Ok(commandItems);
         }
     }
 }
