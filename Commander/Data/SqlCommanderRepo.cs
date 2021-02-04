94f440101c23b10f81b549e946b92c81ee57d1e3
Radeonxz <xuanxxzhao@gmail.com>
2021-02-03 20:49:40 -0500

    chore: add sqlcommandrepo

---
+++ Commander/Data/SqlCommanderRepo.cs
@@ -0,0 +1,15 @@
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        public IEnumberable<Command> GetAllCommands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
