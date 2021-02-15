715b26f8681a9bc4371f107ec2350096cf41bf07
Radeonxz <xuanxxzhao@gmail.com>
2021-02-14 23:28:33 -0500

    chore: add savechanges

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -21,11 +21,11 @@ namespace Commander.Controllers
     {
         // GET api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>(commandItems));
         }
 
         // GET api/commands/{id}
